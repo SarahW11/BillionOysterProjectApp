1ab0288ad64b35b27c5cc07301eec6d3e1bf9061
nnarayanan910 <nnarayanan@ucvts.tec.nj.us>
2015-07-30 12:24:40 -0400

    stff

--- reciept.js
+++ reciept.js
@@ -1,9 +1,48 @@
-var theForm = document.forms["adoptionform"]
+/*using number of oysters requested*/
+function oysters(){
+	var theForm=document.forms["adoptionform"];
+	var quantity=theForm.elelments["number"];
+	var howmany=0;
+	if(quantity.value!=""){
+		howmany=parseInt(quantity.value);
+	}
+	return howmany;
+	var oprice=howmany*0.25;
+}
 
-/*var oysterPrice=*/
 
 /*Get price from various packages*/
-fuction add_Price(){
-	var extra=0;
-	var theForm = document.forms["adoptionform"]
-	var include
+function extraPrice(){
+	var extratotal =0;
+	var theForm = document.forms["adoptionform"];
+	var picture = theForm.elements["picture"];
+    	if(picture.checked==true) {
+        extratotal=extratotal+2;
+   		}
+   	var visit = theForm.elements["visit"];
+    	if(visit.checked==true) {
+        extratotal=extratotal+10;
+   		}
+   	var splush = theForm.elements["small plush"];
+    	if(splush.checked==true) {
+        extratotal=extratotal+5;
+   		}
+   	var jplush = theForm.elements["jumbo plush"];
+    	if(jplush.checked==true) {
+        extratotal=extratotal+10;
+   		}
+   	var bag = theForm.elements["bag"];
+    	if(bag.checked==true) {
+        extratotal=extratotal+2;
+   		}
+   	var video = theForm.elements["video"];
+    	if(video.checked==true) {
+        extratotal=extratotal+5;
+   		}
+    return extratotal;
+}
+
+function totalprice(){
+	var total=oysters()+extraPrice();
+	document.getElementById('totalPrice').innerHTML="Total Price For Cake$"+total;
+}
